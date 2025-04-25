9c8bda05aed997593cd67ffc337e6b6a18cd27ac
Michal <117671671+jerik609@users.noreply.github.com>
2025-04-25 15:29:23 +0200

    and an example on propagate

--- src/main/kotlin/exercises/part5/Exercise5.kt
+++ src/main/kotlin/exercises/part5/Exercise5.kt
@@ -1,11 +1,21 @@
 package org.example.exercises.part5
 
-class Exercise5 {    companion object {
-    fun run() {
+import reactor.core.Exceptions
+import reactor.core.publisher.Flux
 
+class Exercise5 {
 
+    companion object {
+        fun run() {
 
+            Flux.just("1", "2", "three", "4", "5").map {input ->
+                try {
+                    Integer.valueOf(input)
+                } catch (e: NumberFormatException) {
+                    Exceptions.propagate(e)
+                }
+            }.subscribe({println(it)}, {println()})
 
+        }
     }
-}
 }
