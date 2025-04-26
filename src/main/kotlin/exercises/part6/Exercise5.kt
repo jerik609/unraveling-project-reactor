7f38354bd4e2d4195340c0124b7d9bd4bec251b3
Michal <117671671+jerik609@users.noreply.github.com>
2025-04-26 13:56:48 +0200

    part6 exercise5 - example of parallel processing with subsequent merge in one subscriber

--- src/main/kotlin/exercises/part6/Exercise5.kt
+++ src/main/kotlin/exercises/part6/Exercise5.kt
@@ -1,9 +1,30 @@
 package org.example.exercises.part6
 
+import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
+
 class Exercise5 {
     companion object {
         fun run() {
 
+            val task = { value: Int ->
+                val x = 2 * value
+                println("(${Thread.currentThread().name}) - working on: $x")
+                Thread.sleep(5000)
+                println("(${Thread.currentThread().name}) - DONE working on: $x")
+                x
+            }
+
+            Flux.range(1, 10)
+                .parallel() // run in parallel
+                .runOn(Schedulers.parallel())
+                .map { value -> task(value) }
+                .sequential()
+                .publishOn(Schedulers.single())
+                .subscribe(
+                    { println("(${Thread.currentThread().name}) - consumed: $it") },
+                    { println("(${Thread.currentThread().name}) - error: ${it.message}") },
+                    { println("(${Thread.currentThread().name}) - DONE") })
         }
     }
 }
