d88dd1c969551591327fc2b638d905ed16846767
Michal <117671671+jerik609@users.noreply.github.com>
2025-04-26 09:39:15 +0200

    analyzing bubbling exception

--- src/main/kotlin/exercises/part5/Exercise5.kt
+++ src/main/kotlin/exercises/part5/Exercise5.kt
@@ -8,13 +8,38 @@ class Exercise5 {
     companion object {
         fun run() {
 
-            Flux.just("1", "2", "three", "4", "5").map {input ->
-                try {
-                    Integer.valueOf(input)
-                } catch (e: NumberFormatException) {
-                    Exceptions.propagate(e)
+            Flux.just("1", "2", "three", "4", "5")
+                .map { input -> input }
+                .map { input -> input }
+                .onErrorMap { e ->
+                    println("bubbling 2")
+                    e
                 }
-            }.subscribe({println(it)}, {println()})
+                .map { input -> input }
+                .map { input -> input }
+                .onErrorMap { e ->
+                    println("bubbling 1")
+                    e
+                }
+                .map { input ->
+                    try {
+                        Integer.valueOf(input)
+                    } catch (e: NumberFormatException) {
+                        throw Exceptions.bubble(e)
+                        //throw Exceptions.propagate(e)
+                    }
+                }
+                .onErrorMap { e ->
+                    println("propagating 1")
+                    e
+                }
+                .map { input -> input }
+                .map { input -> input }
+                .onErrorMap { e ->
+                    println("propagating 2")
+                    e
+                }
+                .subscribe({println(it)}, { e -> println("ERROR for: ${e.message}")}, )
 
         }
     }
